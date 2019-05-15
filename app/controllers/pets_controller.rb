9ab50fc20b4187a2a79173261219e4bcaaa4b545
Angela <angela.ohh@gmail.com>
2019-05-15 10:47:52 -0700

    created pets#show

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,13 +1,15 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all
+    pets = Pet.all
   end
 
-
+  def show
+    pet = Pet.find_by(id: params[:id])
+  end
 
   private
 
-    def pet_params
-      params.require(:pet).permit(:name, :age, :human)
-    end
+  def pet_params
+    params.require(:pet).permit(:name, :age, :human)
+  end
 end
